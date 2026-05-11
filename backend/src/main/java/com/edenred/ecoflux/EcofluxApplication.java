8eb9534114bc4675686019ab9e5c6f65f1bfc059
joaoeaa <joaoeduardo.andrade03@gmail.com>
2026-05-11 11:21:57 -0300

    Setup inicial do Spring Boot

---
+++ backend/src/main/java/com/edenred/ecoflux/EcofluxApplication.java
@@ -0,0 +1,13 @@
+package com.edenred.ecoflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcofluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcofluxApplication.class, args);
+	}
+
+}

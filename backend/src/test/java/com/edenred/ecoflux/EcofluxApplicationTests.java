bea58217aea824ee280b8f2ad85f5e3e6b898d2e
joaoeaa <joaoeduardo.andrade03@gmail.com>
2026-05-11 11:21:57 -0300

    Setup inicial do Spring Boot

---
+++ backend/src/test/java/com/edenred/ecoflux/EcofluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edenred.ecoflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcofluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
